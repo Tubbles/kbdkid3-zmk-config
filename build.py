011fab3ecb07ae1b10456107ca46be20cfc911f6
Tubbles <jae91m@gmail.com>
2023-09-10 21:19:46 +0200

    Consolidate navigation layer

--- build.py
+++ build.py
@@ -6,48 +6,48 @@ layout = {
     "base":
     [
         """
-        | &kp ESC   | &kp Q    | &kp W      | &kp E    | &kp R     | &kp T     |           |
-        | &kp TAB   | &kp A    | &kp S      | &kp D    | &kp F     | &kp G     |           |
-        | &kp LSHFT | &kp Z    | &kp X      | &kp C    | &kp V     | &kp B     |           |
-        | &kp LCTRL | &kp LGUI | &tog MOUSE | &kp LALT | &mo RAISE | &mo LOWER | &kp SPACE |
+        | &kp ESC   | &kp Q    | &kp W    | &kp E    | &kp R   | &kp T     |           |
+        | &kp TAB   | &kp A    | &kp S    | &kp D    | &kp F   | &kp G     |           |
+        | &kp LSHFT | &kp Z    | &kp X    | &kp C    | &kp V   | &kp B     |           |
+        | &kp LCTRL | &kp LGUI | &kp LALT | &kp LALT | &mo NAV | &mo LOWER | &kp SPACE |
         """,
         """
-        |         | &kp Y     | &kp U     | &kp I     | &kp O     | &kp P     | &kp BSPC  |
-        |         | &kp H     | &kp J     | &kp K     | &kp L     | &kp SEMI  | &kp SQT   |
-        |         | &kp N     | &kp M     | &kp COMMA | &kp DOT   | &kp SLASH | &kp RSHFT |
-        | &kp RET | &mo LOWER | &mo RAISE | &kp RALT  | &tog MISC | &kp RGUI  | &kp RCTRL |
+        |         | &kp Y     | &kp U   | &kp I     | &kp O    | &kp P     | &kp BSPC  |
+        |         | &kp H     | &kp J   | &kp K     | &kp L    | &kp SEMI  | &kp SQT   |
+        |         | &kp N     | &kp M   | &kp COMMA | &kp DOT  | &kp SLASH | &kp RSHFT |
+        | &kp RET | &mo LOWER | &mo NAV | &kp RALT  | &kp RALT | &kp RGUI  | &kp RCTRL |
         """
     ],
 
-    "raise":
+    "nav":
     [
         """
-        | &trans | &kp F1 | &kp F2 | &kp F3 | &kp F4 | &kp F5 |        |
-        | &trans | &none  | &none  | &none  | &none  | &none  |        |
-        | &trans | &none  | &none  | &none  | &none  | &none  |        |
-        | &trans | &trans | &trans | &trans | &trans | &trans | &trans |
+        | &trans | &kp F1 | &kp F2     | &kp F3 | &kp F4 | &kp F5    |        |
+        | &trans | &none  | &none      | &none  | &none  | &kp F11   |        |
+        | &trans | &none  | &none      | &none  | &none  | &kp K_APP |        |
+        | &trans | &trans | &tog MOUSE | &trans | &trans | &trans    | &trans |
         """,
         """
-        |        | &kp F6 | &kp F7    | &kp F8    | &kp F9    | &kp F10 | &kp DEL |
-        |        | &none  | &kp GRAVE | &kp MINUS | &kp EQUAL | &kp F11 | &kp F12 |
-        |        | &none  | &none     | &none     | &none     | &none   | &trans  |
-        | &trans | &trans | &trans    | &trans    | &trans    | &trans  | &trans  |
+        |        | &kp F6    | &kp F7   | &kp F8    | &kp F9    | &kp F10   | &trans  |
+        |        | &kp F12   | &kp LEFT | &kp DOWN  | &kp UP    | &kp RIGHT | &kp INS |
+        |        | &kp K_APP | &kp HOME | &kp PG_DN | &kp PG_UP | &kp END   | &trans  |
+        | &trans | &trans    | &trans   | &trans    | &tog MISC | &trans    | &trans  |
         """
     ],
 
     "lower":
     [
         """
-        | &kp GRAVE | &kp N1 | &kp N2        | &kp N3        | &kp N4            | &kp N5    |        |
-        | &trans    | &none  | &none         | &none         | &ext_power EP_TOG | &kp LBKT  |        |
-        | &trans    | &none  | &kp LC(LS(X)) | &kp LC(LS(C)) | &kp LC(LS(V))     | &kp K_APP |        |
-        | &trans    | &trans | &trans        | &trans        | &trans            | &trans    | &trans |
+        | &kp GRAVE | &kp N1    | &kp N2        | &kp N3        | &kp N4        | &kp N5 |        |
+        | &trans    | &kp PSCRN | &none         | &none         | &none         | &none  |        |
+        | &trans    | &none     | &kp LC(LS(X)) | &kp LC(LS(C)) | &kp LC(LS(V)) | &none  |        |
+        | &trans    | &trans    | &tog MOUSE    | &trans        | &trans        | &trans | &trans |
         """,
         """
-        |        | &kp N6   | &kp N7   | &kp N8    | &kp N9    | &kp N0    | &kp INS  |
-        |        | &kp RBKT | &kp LEFT | &kp DOWN  | &kp UP    | &kp RIGHT | &kp BSLH |
-        |        | &none    | &kp HOME | &kp PG_DN | &kp PG_UP | &kp END   | &trans   |
-        | &trans | &trans   | &trans   | &trans    | &trans    | &trans    | &trans   |
+        |        | &kp N6    | &kp N7    | &kp N8    | &kp N9    | &kp N0   | &kp DEL  |
+        |        | &kp GRAVE | &kp MINUS | &kp EQUAL | &kp LBKT  | &kp RBKT | &kp BSLH |
+        |        | &none     | &none     | &none     | &kp LT    | &kp GT   | &trans   |
+        | &trans | &trans    | &trans    | &trans    | &tog MISC | &trans   | &trans   |
         """
     ],
 
@@ -56,40 +56,40 @@ layout = {
         """
         | &to CONF | &none      | &none      | &none        | &none        | &none      |        |
         | &kp C_PP | &kp C_PREV | &kp C_NEXT | &kp C_VOL_DN | &kp C_VOL_UP | &kp C_MUTE |        |
-        | &trans   | &kp PSCRN  | &none      | &none        | &none        | &none      |        |
-        | &trans   | &trans     | &trans     | &trans       | &trans       | &trans     | &trans |
+        | &trans   | &none      | &none      | &none        | &none        | &none      |        |
+        | &trans   | &trans     | &tog MISC  | &trans       | &trans       | &trans     | &trans |
         """,
         """
-        |              | &kp KP_NLCK | &kp KP_N7 | &kp KP_N8 | &kp KP_N9 | &kp KP_DIVIDE   | &kp KP_MINUS |
-        |              | &none       | &kp KP_N4 | &kp KP_N5 | &kp KP_N6 | &kp KP_MULTIPLY | &kp KP_PLUS  |
+        |              | &kp KP_NLCK | &kp KP_N7 | &kp KP_N8 | &kp KP_N9 | &kp KP_MULTIPLY | &kp KP_MINUS |
+        |              | &none       | &kp KP_N4 | &kp KP_N5 | &kp KP_N6 | &kp KP_DIVIDE   | &kp KP_PLUS  |
         |              | &kp KP_N0   | &kp KP_N1 | &kp KP_N2 | &kp KP_N3 | &kp KP_DOT      | &trans       |
-        | &kp KP_ENTER | &trans      | &trans    | &trans    | &trans    | &trans          | &trans       |
+        | &kp KP_ENTER | &trans      | &trans    | &trans    | &tog MISC | &trans          | &trans       |
         """
     ],
 
     "mouse":
     [
         """
-        | &to CONF | &none  | &none  | &none  | &none  | &none  |        |
-        | &trans   | &none  | &none  | &none  | &none  | &none  |        |
-        | &trans   | &none  | &none  | &none  | &none  | &none  |        |
-        | &trans   | &trans | &trans | &trans | &trans | &trans | &trans |
+        | &to CONF | &none  | &none      | &none  | &none  | &none  |        |
+        | &trans   | &none  | &none      | &none  | &none  | &none  |        |
+        | &trans   | &none  | &none      | &none  | &none  | &none  |        |
+        | &trans   | &trans | &tog MOUSE | &trans | &trans | &trans | &trans |
         """,
         """
-        |        | &none  | &none  | &none  | &none  | &none  | &trans |
-        |        | &none  | &none  | &none  | &none  | &none  | &none  |
-        |        | &none  | &none  | &none  | &none  | &none  | &trans |
-        | &trans | &trans | &trans | &trans | &trans | &trans | &trans |
+        |        | &none  | &none  | &none  | &none      | &none  | &trans |
+        |        | &none  | &none  | &none  | &none      | &none  | &none  |
+        |        | &none  | &none  | &none  | &none      | &none  | &trans |
+        | &trans | &trans | &trans | &trans | &tog MOUSE | &trans | &trans |
         """
     ],
 
     "conf":
     [
         """
-        | &to BASE | &bt BT_SEL 0 | &bt BT_SEL 1 | &bt BT_SEL 2 | &bt BT_SEL 3 | &bt BT_SEL 4 |       |
-        | &none    | &bt BT_CLR   | &none        | &none        | &none        | &sys_reset   |       |
-        | &none    | &out OUT_BLE | &out OUT_USB | &none        | &none        | &bootloader  |       |
-        | &none    | &none        | &none        | &none        | &none        | &none        | &none |
+        | &to BASE | &bt BT_SEL 0 | &bt BT_SEL 1 | &bt BT_SEL 2 | &bt BT_SEL 3      | &bt BT_SEL 4 |       |
+        | &none    | &bt BT_CLR   | &none        | &none        | &ext_power EP_TOG | &sys_reset   |       |
+        | &none    | &out OUT_BLE | &out OUT_USB | &none        | &none             | &bootloader  |       |
+        | &none    | &none        | &none        | &none        | &none             | &none        | &none |
         """,
         """
         |       | &none       | &none | &none | &none | &none | &to BASE |
